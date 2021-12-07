11f5c27aa82597c0eb50b2d424d6de31cf7e5df6
Ivonne Kneißig <kneissig.ivonne@fh-swf.de>
2021-12-07 21:27:48 +0100

    Datenbankanbindung

---
+++ src/main/java/de/fhswf/in/informatik/se/projektverwaltung/ProjektverwaltungApplication.java
@@ -0,0 +1,14 @@
+package de.fhswf.in.informatik.se.projektverwaltung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektverwaltungApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjektverwaltungApplication.class, args);
+        System.out.println("Yeah man, es läuft! :3");
+    }
+
+}
